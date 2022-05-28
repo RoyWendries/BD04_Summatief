a91770d8e62dd37eba124714045bedaa93c490cf
RoyWendries <1747290wendries@zuyd.nl>
2022-05-28 14:04:45 +0200

    Fixed names and added actual predictions

--- Fase_1_MLR-LR/Fase1_MLR-ML.py
+++ Fase_1_MLR-LR/Fase1_MLR-ML.py
@@ -26,9 +26,9 @@ class MLModel:
 
     # Set data to make predicions on
     def predict_Data(self, data1, data2, data3):
-        self.Xpredict1 = data1
-        self.Xpredict2 = data2
-        self.Xpredict3 = data3
+        self.Xpredict1 = [data1]
+        self.Xpredict2 = [data2]
+        self.Xpredict3 = [data3]
 
     # Fit data to model
     def Regfitter(self):
@@ -46,7 +46,7 @@ class MLModel:
         predictions = [self.Xpredict1, self.Xpredict2, self.Xpredict3]
         for e in predictions:
             print('Predicted', predict, 'of a car on the basis of a/an', st,
-                  'of', e[0], 'and a/an', nd, 'of', e[1])
+                  'of', e[0][0], 'and a/an', nd, 'of', e[0][1], ': ', self.data.predict(e))
 
 
 # Calling MLR
@@ -58,9 +58,9 @@ mlr.predict_Data([5, 100], [15, 100000], [20, 30000])
 mlr.results('cost', 'age', 'distance traveled')
 
 # Calling ML
-ml = MLModel(LogisticRegression, 'ML')
-ml.readData()
-ml.df_Setter(['Mfg_Year', 'Price'], 'Fuel_Type')
-ml.split_Data()
-ml.predict_Data([1998, 9000], [1999, 8671], [2002, 13500])
-ml.results('fuel type', 'build year', 'cost')
+lr = MLModel(LogisticRegression, 'LR')
+lr.readData()
+lr.df_Setter(['Mfg_Year', 'Price'], 'Fuel_Type')
+lr.split_Data()
+lr.predict_Data([1998, 9000], [1999, 8671], [2002, 13500])
+lr.results('fuel type', 'build year', 'cost')
